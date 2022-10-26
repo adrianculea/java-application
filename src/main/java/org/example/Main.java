7d64c6754e12d10d621515639c344977d13900fe
Adrian Culea <adrian.culea@ebs.msft>
2022-10-26 09:08:01 +0300

    update output text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Buna dimineata!!");
     }
 }
