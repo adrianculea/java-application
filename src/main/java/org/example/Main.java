ab4f9c3cf1eff9ab2e4a1553bf7099181b4dec96
Adrian Culea <adrian.culea@ebs.msft>
2022-10-26 08:17:03 +0300

    update hello world text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hellooooo!!");
+        System.out.println("Hello World!!");
     }
 }
